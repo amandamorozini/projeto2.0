17fdd0beb3deacb0c117fe8be6d67835771b90bb
amandamorozini <amanda.morozini@escola.pr.gov.br>
2026-08-03 11:24:13 -0300

    2

--- script.js
+++ script.js
@@ -2,5 +2,16 @@ const botoes = document.qurySelectorAll ("button");
 
 botoes.forEach (function (botao)) {
     let curtiu = false;
-    botao.addEventlistener ("click", botaoClicado)
+    botao.addEventlistener ("click", botaoClicado);
+    function botaoClicado () {
+        console.log ("fui clicado");
+        let texto = botao.qurySelector ("span");
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;     
+        } else {
+            texto.textContent--;
+            curtiu= false;
+        }
+    }
 }
